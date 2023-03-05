95b16f785ed830e81769a543fa08bd637dffb530
roshandelpoor <roshandelpoor@outlook.com>
2023-03-05 11:32:36 +0330

    add function generateRandomToken

--- src/Tools.php
+++ src/Tools.php
@@ -156,4 +156,19 @@ class Tools
             }
         }
     }
+
+    function generateRandomToken($length)
+    {
+        $token        = "";
+        $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $codeAlphabet .= "abcdefghijklmnopqrstuvwxyz";
+        $codeAlphabet .= "0123456789";
+        $max          = strlen($codeAlphabet); // edited
+
+        for ($i = 0; $i < $length; $i++) {
+            $token .= $codeAlphabet[crypto_rand_secure(0, $max - 1)];
+        }
+
+        return $token;
+    }
 }
