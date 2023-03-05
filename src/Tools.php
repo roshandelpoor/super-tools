e7a440b9b1a346c94502b6b45660b6011d516038
roshandelpoor <roshandelpoor@outlook.com>
2023-03-05 11:16:50 +0330

    add function englishToPersianNumber and base64ToImage

--- src/Tools.php
+++ src/Tools.php
@@ -6,9 +6,55 @@ class Tools
 {
     function persianToEnglishNumber($number)
     {
-        $persianNumbers = ['۰','۱','۲','۳','۴','۵','۶','۷','۸','۹'];
-        $englishNumbers = ['0','1','2','3','4','5','6','7','8','9'];
-
-        return str_replace($persianNumbers, $englishNumbers, $number);
+        return strtr($string, [
+            '۰' => '0',
+            '۱' => '1',
+            '۲' => '2',
+            '۳' => '3',
+            '۴' => '4',
+            '۵' => '5',
+            '۶' => '6',
+            '۷' => '7',
+            '۸' => '8',
+            '۹' => '9',
+            '٠' => '0',
+            '١' => '1',
+            '٢' => '2',
+            '٣' => '3',
+            '٤' => '4',
+            '٥' => '5',
+            '٦' => '6',
+            '٧' => '7',
+            '٨' => '8',
+            '٩' => '9',
+        ]);
     }
+
+    function englishToPersianNumber($number)
+    {
+        return strtr($string, [
+            '0' => '۰',
+            '1' => '۱',
+            '2' => '۲',
+            '3' => '۳',
+            '4' => '۴',
+            '5' => '۵',
+            '6' => '۶',
+            '7' => '۷',
+            '8' => '۸',
+            '9' => '۹',
+        ]);
+
+    }
+
+    function base64ToImage($base64_string, $output_file)
+    {
+        $file = fopen($output_file, "wb");
+        fwrite($file, base64_decode($base64_string));
+        fclose($file);
+
+        return $output_file;
+    }
+
+    
 }
